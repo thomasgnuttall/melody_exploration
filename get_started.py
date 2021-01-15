6babc6858ff537f1b5d3dc478441c21d37b390ca
thomasgnuttall <thomasgnuttalll@gmail.com>
2021-01-15 21:46:22 +0100

    First push

---
+++ get_started.py
@@ -0,0 +1,19 @@
+import mirdata
+
+carnatic = mirdata.initialize('saraga_carnatic')
+carnatic.download()
+
+hindustani = mirdata.initialize('saraga_carnatic')
+hindustani.download()
+
+## Data Load ##
+
+
+## Pitch Extraction (if not exists) ##
+
+
+## Melodic Analysis ##
+
+# part b
+
+# part b
